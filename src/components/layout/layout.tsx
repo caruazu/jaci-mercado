4f20b920499251af182133ab609364eeefbef48e
caruazu <gustavocaruazu@gmail.com>
2025-07-17 19:03:33 -0300

    segmentação do layout

---
+++ src/components/layout/layout.tsx
@@ -0,0 +1,14 @@
+import { Header } from '../header';
+
+type LayoutProps = {
+  children: React.ReactNode;
+};
+
+export const Layout = ({ children }: LayoutProps) => {
+  return (
+    <div className="dark relative flex min-h-screen flex-col">
+      <Header />
+      <main className="mv-12 flex flex-1 flex-col">{children}</main>
+    </div>
+  );
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,8 +15,6 @@ export default function Home() {
   return (
     <div
       className={`${geistSans.variable} ${geistMono.variable} grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20`}
-    >
-      <Header />
-    </div>
+    ></div>
   );
 }
